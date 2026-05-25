8d92da1a4b9f48f89f4411f92939f4fb21aa2e93
Amit Kesarwani <amitkesarwani.9309@gmail.com>
2026-05-26 01:50:50 +0530

    Fix Railway host issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['esg-frontend-production-d25a.up.railway.app']
+  }
 })
